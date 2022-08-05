2f3cb83b69af6ce5fbdd249950bc2259ed4ba924
every-sun <everysun@amuz.co.kr>
2022-08-05 14:07:29 +0900

    style : tailwindcss 탭메뉴 적용

---
+++ src/components/WithTabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pb-5 border-b border-gray-200 sm:pb-0">
+    <h3 class="text-lg leading-6 font-medium text-gray-900">게시글 목록</h3>
+    <div class="mt-3 sm:mt-4">
+      <div class="sm:hidden">
+        <label for="current-tab" class="sr-only">Select a tab</label>
+        <select id="current-tab" name="current-tab" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+          <option v-for="tab in props.tabItems" :key="tab" :selected="tab">{{ tab.value }}</option>
+        </select>
+      </div>
+      <div class="hidden sm:block">
+        <nav class="-mb-px flex space-x-8">
+          <a v-for="tab in props.tabItems" :key="tab.value" :class="[props.currentTab === tab.value ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm cursor-pointer']" :aria-current="tab ? 'page' : undefined" @click="$emit('onTabItemClick', tab.value)">
+            {{ tab.name }}
+          </a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+const props = defineProps({
+  tabItems : {
+    type: Object
+  },
+  currentTab: {
+    type: String
+  }
+})
+
+</script>
--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -1,14 +1,17 @@
 <template>
   <h1>{{ $route.params.userId }} 님의 게시글 목록</h1>
-  <div class="flex mb-[30px] justify-around rounded-lg overflow-hidden">
-    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='all'}" @click="onTabMenuClick">전체</button>
-    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='false'}" @click="onTabMenuClick">작성중</button>
-    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='true'}" @click="onTabMenuClick">완료</button>
-  </div>
-  <ul v-if="tabMenuStatus==='all'">
+
+  <WithTabs :tabItems="tabItems" :currentTab="currentTab" @onTabItemClick="onTabItemClick" />
+
+<!--  <div class="flex mb-[30px] justify-around rounded-lg overflow-hidden">-->
+<!--    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='all'}" @click="onTabMenuClick">전체</button>-->
+<!--    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='false'}" @click="onTabMenuClick">작성중</button>-->
+<!--    <button :class="{ 'w-[34%]':true, 'bg-sky-400' : tabMenuStatus==='true'}" @click="onTabMenuClick">완료</button>-->
+<!--  </div>-->
+  <ul v-if="currentTab==='all'">
     <list-item v-for="post in allList" :key="post.id" :item="{userId: $route.params.userId, postId: post.id, print: post.title }" component="PostDetail" />
   </ul>
-  <ul v-else-if="tabMenuStatus==='false'">
+  <ul v-else-if="currentTab==='false'">
     <list-item v-for="post in proceedingList" :key="post.id" :item="{userId: $route.params.userId, postId: post.id, print: post.title }" component="PostDetail" />
   </ul>
   <ul v-else>
@@ -22,11 +25,13 @@
   import { useRoute } from 'vue-router';
   import ListItem from "@/components/ListItem";
   import {router} from "@/router";
+  import WithTabs from "@/components/WithTabs";
 
   const store = useStore();
   const route = useRoute();
 
-  let tabMenuStatus = ref('all');
+  const tabItems = [{value: 'all', name: '전체'}, {value: 'false', name: '작성중'}, {value:'true', name:'작성완료'} ];
+  const currentTab = ref('all');
 
   const dataList = store.state.todoList.data;
 
@@ -34,15 +39,9 @@
   const proceedingList =  dataList.filter(v=> v.userId===Number(route.params.userId) && v.completed===false);
   const completedList = dataList.filter(v=> v.userId===Number(route.params.userId) && v.completed===true);
 
-  const onTabMenuClick = event => {
-    const state = event.target.innerText;
-    if(state==='전체'){
-      tabMenuStatus.value = 'all';
-    }else if(state==='작성중'){
-      tabMenuStatus.value = 'false';
-    }else{
-      tabMenuStatus.value = 'true';
-    }
+  const onTabItemClick = item => {
+    currentTab.value = item;
+    console.log(item, currentTab.value);
   }
 
   const goToList =  () => {
