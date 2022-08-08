7952b32483b69f2a191c81420898d0b3cf312c80
every-sun <everysun@amuz.co.kr>
2022-08-08 17:09:28 +0900

    feat : 댓글 수정

--- src/components/CommentListItemGroup.vue
+++ src/components/CommentListItemGroup.vue
@@ -19,21 +19,29 @@
               <p class="text-sm text-gray-500 truncate">
                 {{  comment.body }}
               </p>
+              <div class="mt-4">
               <button type="button"
+                      v-if="comment.id!==targetId"
+                      @click="editComment(comment)"
                       class="inline-flex items-center border border-transparent text-[12px] rounded-md shadow-sm mx-0.5 text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">수정</button>
               <button type="button"
+                      v-if="comment.id!==targetId"
                       @click="deleteComment(comment.id)"
                       class="inline-flex items-center border border-transparent text-[12px] font-medium rounded-md mx-0.5 shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">삭제</button>
-
+              </div>
             </div>
           </div>
+          <form v-if="comment.id===targetId" class="mb-4">
+            <textarea rows="3" class="block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm" v-model="message"></textarea>
+            <button type="submit" @click.prevent="updateComplete(comment)" class="inline-flex items-center border border-transparent text-[12px] font-medium rounded-md mx-0.5 shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">완료</button>
+          </form>
         </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import {reactive, watch} from "vue";
+import {ref, reactive, watch} from "vue";
 import { useRoute } from 'vue-router';
 import {fetchCommentList} from "@/api";
 import Post from "@/models/Post";
@@ -42,6 +50,9 @@ import AddCommentForm from "@/components/AddCommentForm";
 
 const route = useRoute();
 const commentList = reactive({value: []})
+
+const message = ref('');
+const targetId = ref(0);
 
 if(Comment.all().length===0 || Comment.all()[0].postId!==Number(route.params.id)){
   const fetchCommentData = await fetchCommentList(route.params.id);
@@ -87,5 +98,21 @@ const deleteComment = (id) => {
   commentList.value = Post.query().where('id', Number(route.params.id)).with('comments').first()?.comments;
 }
 
+const editComment = (target) => {
+  targetId.value = target.id;
+  message.value = target.body;
+}
+
+const updateComplete = (target) => {
+  console.log(target);
+  Comment.update({
+    where: target.id,
+    data: {
+      body: message.value
+    }
+  })
+  targetId.value = 0;
+  commentList.value = Post.query().where('id', Number(route.params.id)).with('comments').first()?.comments;
+}
 
 </script>
